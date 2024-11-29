80975287a559dcb63b7eac639c31aff00834061b
schwienernitzel <pfelix0803@gmail.com>
2024-11-29 22:14:27 +0000

    wiki: build: Add new `check.sh`
    Signed-off-by: schwienernitzel <pfelix0803@gmail.com>

---
+++ build/check.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Device-specific repositories
+cd device/huawei/liszt && git status && cd ../../../ &&
+cd kernel/huawei/liszt && git status && cd ../../../ &&
+cd vendor/huawei/liszt && git status && cd ../../../ &&
+
+# Device-specific patches
+cd patches && git status && cd ../ &&
+
+# Hardware repositories
+cd hardware/huawei && git status && cd ../../ &&
+
+# External stlport library
+cd external/stlport && git status && cd ../../
