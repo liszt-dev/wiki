feeb8dcb88cd539dd2cba455be8a2d6a173a3176
schwienernitzel <pfelix0803@gmail.com>
2024-11-29 22:02:42 +0000

    wiki: build: Add new `setup.sh`
    Signed-off-by: schwienernitzel <pfelix0803@gmail.com>

---
+++ build/setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+ORG="https://github.com/liszt-dev"
+BRANCH="-b cm-13.0"
+
+# Device-specific repositories
+git clone $ORG/android_device_huawei_liszt $BRANCH device/huawei/liszt
+git clone $ORG/android_kernel_huawei_liszt $BRANCH kernel/huawei/liszt
+git clone $ORG/android_vendor_huawei_liszt $BRANCH vendor/huawei/liszt
+
+# Device-specific patches
+git clone $ORG/patches $BRANCH patches
+
+# Hardware repositories
+git clone $ORG/android_hardware_huawei $BRANCH hardware/huawei
+
+# External stlport library
+git clone $ORG/android_external_stlport $BRANCH external/stlport
